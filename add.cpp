4ccb82d982b68ef19f8b50d3dd91c9cd27bc8458
kowsik72 <111626363+kowsik72@users.noreply.github.com>
2022-08-20 10:54:47 +0530

    Add files via upload

---
+++ add.cpp
@@ -0,0 +1,11 @@
+//adding two numbers using c++
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "enter two numbers:"<<endl;
+    int v1,v2;
+     cin >> v1 >> v2;
+     cout<< " the mul of "<<v1<<" and "<<v2
+          <<" is " << v1*v2<< endl;
+    return 0;
+    }
